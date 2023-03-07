8dd56fc8916be1c18bed5fb8ce496afca1d0e27d
wangboyang <wangboyang@datagrand.com>
2023-03-07 17:12:49 +0800

    feat(app): push App.tsx change

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { useEffect } from 'react';
 import createExample from './game/phaserExample';
 import createFirstGame from './game/firstGame';
+import loadTexturePacker from './game/loadTexturePacker';
+import { events, gameObjects } from './familiarPhaserApi';
 import './App.css';
 
 function App() {
   useEffect(() => {
     // createExample();
-    createFirstGame();
+    // createFirstGame();
+    // loadTexturePacker();
+    // events();
+    gameObjects();
   }, []);
 
   return <div />;
